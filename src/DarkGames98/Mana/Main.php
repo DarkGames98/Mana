94d1b48aef92bd58fdf81f29e577d33561ff4229
DarkGames98 <87958211+DarkGames98@users.noreply.github.com>
2021-07-25 22:49:23 -0400

    Create Main.php

---
+++ src/DarkGames98/Mana/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DarkGames98\Mana;
+
+use pocketmine\{
+	plugin\PluginBase,
+	event\Listener,
+	event\block\BlockBreakEvent,
+};
+use twisted\multieconomy\MultiEconomy;
+class Main extends PluginBase implements Listener
+{
+	public function onEnable(){
+		$this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onBlockEvent(BlockBreakEvent $event):void{
+		$player = $event->getPlayer();
+		$block = $event->getBlock();
+		if($event->isCancelled()) return;
+		if(isset($this->getConfig()->getAll()[$block->getID().":".$block->getDamage()])){
+			$multieconomy = MultiEconomy::getInstance()->getCurrency("Mana")->addToBalance($player->getName(), $this->getConfig()->getAll()[$block->getID().":".$block->getDamage()]);
+		}elseif(isset($this->getConfig()->getAll()[$block->getID()])){
+			$multieconomy = MultiEconomy::getInstance()->getCurrency("Mana")->addToBalance($player->getName(), $this->getConfig()->getAll()[$block->getID()]);
+		}
+	}
+}
